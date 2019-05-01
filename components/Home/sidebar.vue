8a66757fe5e3ab17fc2c03df0e92f9817fdb8946
SimpleThread <simplethread@163.com>
2019-05-01 19:47:28 +0800

    cx

--- components/Home/sidebar.vue
+++ components/Home/sidebar.vue
@@ -4,9 +4,8 @@
       :style="{ backgroundImage: 'url(' + indexImg + ')' }" 
       class="cover-img"/>
     <div class="bottom-block">
-      <h1>C X</h1>
-      <h3>有朋自远方来</h3>
-      <h3>不亦乐乎</h3>
+      <h3>但行好事</h3>
+      <h3>莫问前程</h3>
     </div>
   </div>
 </template>
