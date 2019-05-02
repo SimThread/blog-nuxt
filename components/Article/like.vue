ed24d61a4212aeb4650c71f11c55563455a4f20e
SimpleThread <simplethread@163.com>
2019-05-02 14:18:45 +0800

    cx

--- components/Article/like.vue
+++ components/Article/like.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="article-share clearfix">
-    <div
+    <a 
       :class=" isLike ? 'note-liked' :'' " 
+      href="javascript:;" 
       class="like-btn" 
-      @click="toggleLike($route.params.id)">
+      @click.prevent="toggleLike($route.params.id)">
       <span class="like-content">
         <i 
           :class="isLike ? 'fa-heart' : 'fa-heart-o'" 
           class="fa"/>  喜欢
       </span>
       <span class="like-count">{{ likeCount }}</span>        
-    </div>
+    </a>
   </div>
 </template>
 <script>
