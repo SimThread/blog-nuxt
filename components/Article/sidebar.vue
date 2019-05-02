eeeb784d991b4d34c3824960649d6f9ee737adde
SimpleThread <simplethread@163.com>
2019-05-02 14:09:45 +0800

    cx

--- components/Article/sidebar.vue
+++ components/Article/sidebar.vue
@@ -9,16 +9,16 @@
 export default {
   mounted() {
     this.$nextTick(() => {
-      document.addEventListener('click', e => {
-        console.log('outer')
-        if (e.target.classList.contains('toc-link')) {
-          console.log('toc-link')
-          e.preventDefault()
-          const selector = e.target.getAttribute('href').slice(1)
-          var anchor = document.getElementById(selector)
-          document.documentElement.scrollTop = anchor.offsetTop
-        }
-      })
+      // document.addEventListener('click', e => {
+      //   console.log('outer')
+      //   if (e.target.classList.contains('toc-link')) {
+      //     console.log('toc-link')
+      //     e.preventDefault()
+      //     const selector = e.target.getAttribute('href').slice(1)
+      //     var anchor = document.getElementById(selector)
+      //     document.documentElement.scrollTop = anchor.offsetTop
+      //   }
+      // })
     })
   }
 }
