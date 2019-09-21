239ae2e3ef24d1b7b191e757ede646d07225bf04
SimpleThread <simplethread@163.com>
2019-09-22 06:57:45 +0800

    cx

--- nuxt.config.js
+++ nuxt.config.js
@@ -139,7 +139,7 @@ module.exports = {
     [
       '/online/api',
       {
-        target: '//39.106.73.251:8800',
+        target: 'http://39.106.73.251:8800',
         pathRewrite: {
           '^/online/api': ''
         }
