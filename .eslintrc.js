427224c4b7ee916201b999a90693fdd310e24275
chenxiong <chenxiong@addcn.com>
2019-09-29 20:03:42 +0800

    cx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,46 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    // 'plugin:vue/recommended',
-    // 'plugin:prettier/recommended',
-    '@nuxtjs/eslint-config-typescript'
-  ],
-  // required to lint *.vue files
-  plugins: [
-    'vue',
-    'prettier'
-  ],
-  // add your custom rules here
-  rules: {
-    "indent": ["error", 4],
-    "semi": [2, "always"],
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    extends: [
+        '@nuxtjs/eslint-config-typescript',
+        'plugin:vue/recommended'
+        // 'plugin:prettier/recommended',
+    ],
+    // required to lint *.vue files
+    plugins: [
+        'vue'
+        // 'prettier'
+    ],
+    // add your custom rules here
+    rules: {
+        indent: ['error', 4],
+        semi: [2, 'always'],
+        'vue/max-attributes-per-line': [
+            2,
+            {
+                singleline: 2,
+                multiline: {
+                    max: 2,
+                    allowFirstLine: false
+                }
+            }
+        ],
+        'vue/html-indent': [
+            'error',
+            4,
+            {
+                attribute: 1,
+                closeBracket: 0,
+                alignAttributesVertically: true,
+                ignores: []
+            }
+        ],
+        'vue/require-v-for-key': 'off'
+    }
 }
